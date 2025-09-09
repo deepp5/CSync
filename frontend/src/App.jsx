11c258c1e3357e079f561dac92bdcee1ee381462
Jay Patel <itsjay0730@gmail.com>
2025-09-08 23:30:18 -0500

    updated aurora speed + amplitude

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,8 +31,8 @@ export default function App() {
       <Aurora
         colorStops={['#fa4efd', '#9172f8', '#21daf2']}
         blend={0.5}
-        amplitude={1.0}
-        speed={0.5}
+        amplitude={1.15}
+        speed={0.6}
       />
       <NavBar/>
       <HomePage />
