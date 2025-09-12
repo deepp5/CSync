fec99d3619a4d4d881e0d6227de39b7f45789d0c
Jay Patel <itsjay0730@gmail.com>
2025-09-11 22:57:28 -0500

    Added react router, only routed landing page so far

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 
 export default function App() {
-  const [users, setUsers] = useState([]);
+  //IDK WHAT ANY OF THIS IS??? ------------------------
+  // const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/users")
+  //     .then((res) => res.json())
+  //     .then((data) => setUsers(data));
+  // }, []);
+  //---------------------------------------------------
 
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />}/>
+      </Routes>
+    </Router>
 
    
     
