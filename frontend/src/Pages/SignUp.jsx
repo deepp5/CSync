13fafc41e91e116b71144d62281a0807d3893d09
deepp5 <deepnpatel05@gmail.com>
2025-09-16 22:50:53 -0500

    Update SignUp.jsx

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Aurora from "../Components/LandingPage/Aurora";
-import LoginForm from "../Components/SignIn/SignUp"
-export default function SignUp(){
-    return(
-        <div>
-            <Aurora
-                colorStops={['#fa4efd', '#9172f8', '#21daf2']}
-                blend={0.5}
-                amplitude={1.15}
-                speed={0.6}
-            />
-            <LoginForm/>
-        </div>
-    );
+import LoginForm from "../Components/SignIn/SignUp";
+export default function SignUp() {
+  return (
+    <div>
+      <Aurora
+        colorStops={["#fa4efd", "#9172f8", "#21daf2"]}
+        blend={0.5}
+        amplitude={1.15}
+        speed={0.6}
+      />
+      <LoginForm />
+    </div>
+  );
 }
