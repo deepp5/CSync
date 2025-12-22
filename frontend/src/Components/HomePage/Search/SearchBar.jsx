b373861c0fe2802253f73b39be6db30a584ea081
Jay Patel <itsjay0730@gmail.com>
2025-12-21 23:27:25 -0600

    added search icon in home

--- frontend/src/Components/HomePage/Search/SearchBar.jsx
+++ frontend/src/Components/HomePage/Search/SearchBar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 import "./SearchBar.css";
 
 export default function SearchBar() {
     return (
         <div className="search-container">
-            <input
-                className="search-input"
-                placeholder="Search projects..."
-                type="text"
-            />
+            <div className="search-bar-wrapper">
+                <FiSearch className="search-icon" />
+                <input
+                    className="search-input"
+                    placeholder="Search projects..."
+                    type="text"
+                />
+            </div>
             <button className="filter-btn">Filters</button>
         </div>
     );
