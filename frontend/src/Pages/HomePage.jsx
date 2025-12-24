53899631d6d1fca20956fb1e7da17afa223262dd
Jay Patel <itsjay0730@gmail.com>
2025-12-23 18:58:27 -0600

    w

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -52,9 +52,12 @@ export default function HomePage() {
       <Sidebar />
 
       <div className="home-content">
+        <h1 className="home-title">Browse Projects</h1>
         <SearchBar />
         <Grid posts={posts} />
       </div>
     </div>
   );
 }
+
+
