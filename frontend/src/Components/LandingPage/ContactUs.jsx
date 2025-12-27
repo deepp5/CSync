545e0605c8e122247fc897f9d6ec7b425e6f0ba7
pvishrut05 <vp829252@gmail.com>
2025-12-26 18:54:45 -0600

    Added a new Features section.

--- frontend/src/Components/LandingPage/ContactUs.jsx
+++ frontend/src/Components/LandingPage/ContactUs.jsx
@@ -70,7 +70,7 @@ export default function ContactUs() {
             {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"> */}
               {/* <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /> */}
             {/* </svg> */}
-            <a href="vp829252@gmail.com">vp829252@gmail.com</a>
+            <a href="csyncteam@gmail.com">csyncteam@gmail.com</a>
           </div>
 
           {/* Social Icons */}
