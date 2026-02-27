b6254d39bc9062aa08f3ed2f549ca49831c5347f
Jay Patel <itsjay0730@gmail.com>
2026-02-27 01:41:50 -0600

    update

--- frontend/src/Components/MyProject/MyProject.jsx
+++ frontend/src/Components/MyProject/MyProject.jsx
@@ -19,7 +19,7 @@ import {
   FiShare2,
   FiLock,
 } from "react-icons/fi";
-const API_BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:5051";
+
 
 export default function MyProjects() {
   const navigate = useNavigate();
